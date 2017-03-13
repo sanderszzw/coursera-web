a0c8b0b42c042dab14ee5e3a6c6335dc34277f0b
Zhiwen Zhang <sanderszzw@hotmail.com>
2017-03-13 16:37:14 -0700

    merge assignment5

---
+++ assign5/src/restaurant.module.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function() {
+  /**
+   * Restaurant module that includes the public module as a dependency
+   */
+  angular.module('restaurant', ['public'])
+  .config(config);
+
+  config.$inject = ['$urlRouterProvider'];
+  function config($urlRouterProvider) {
+
+    // If user goes to a path that doesn't exist, redirect to public root
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
